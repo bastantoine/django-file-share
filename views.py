c878680ea5d50f9a3826dfec7ac57dd250eda62d
bastantoine <baba_cmoi@hotmail.fr>
2020-02-26 17:41:21 +0100

    Added views and base classes

--- views.py
+++ views.py
@@ -2,7 +2,33 @@ from django.shortcuts import render
 from django.views import View
 
 
-class HomeView(View):
+class BaseView(View):
+    pass
+
+
+class ProtectedView(BaseView):
+    pass
+
+
+class HomeView(BaseView):
 
     def get(self, request):
         return render(request, 'file_explorer/home.html')
+
+
+class AdminView(ProtectedView):
+
+    def get(self, request):
+        return render(request, 'file_explorer/admin.html')
+
+
+class UploadFileView(BaseView):
+
+    def get(self, request):
+        return render(request, 'file_explorer/upload.html')
+
+
+class GetFileView(BaseView):
+
+    def get(self, request):
+        return render(request, 'file_explorer/get.html')
