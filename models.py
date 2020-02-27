ca9ee988659dc080c1085d2707540221454df96d
bastantoine <baba_cmoi@hotmail.fr>
2020-02-27 10:18:36 +0100

    Added model for uploaded file

--- models.py
+++ models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class UploadedFile(models.Model):
+
+    uploaded_file = models.FileField(upload_to=None, max_length=100)
+    password = models.CharField(max_length=20)
+    limit_time = models.DateField()
+    uuid = models.UUIDField()
+    available = models.BooleanField()
