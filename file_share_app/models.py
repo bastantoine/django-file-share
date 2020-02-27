7847079acfe7fe823d5a5c41703755a6c7337577
bastantoine <baba_cmoi@hotmail.fr>
2020-02-27 10:35:40 +0100

    Making password from file not required

--- file_share_app/models.py
+++ file_share_app/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class UploadedFile(models.Model):
 
     uploaded_file = models.FileField(upload_to=None, max_length=100)
-    password = models.CharField(max_length=20)
+    password = models.CharField(max_length=20, blank=True)
     limit_time = models.DateField()
     uuid = models.UUIDField()
     available = models.BooleanField()
