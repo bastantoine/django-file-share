fc2682df2f521f6f9c7109e4331113dba661b0bd
bastantoine <baba_cmoi@hotmail.fr>
2020-03-01 23:27:54 +0100

    Updated upload destination for the files

--- file_share_app/models.py
+++ file_share_app/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class UploadedFile(models.Model):
 
-    uploaded_file = models.FileField(upload_to=None, max_length=100)
+    uploaded_file = models.FileField(upload_to='upload/%Y/%m/%d/', max_length=100)
     password = models.CharField(max_length=20, blank=True)
     limit_time = models.DateField()
     uuid = models.UUIDField(default=uuid.uuid4)
