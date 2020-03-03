2d091b36cac1fcf6736777d1fa67354037cb4cf2
bastantoine <baba_cmoi@hotmail.fr>
2020-03-03 14:49:59 +0100

    Making sure the namespace is included in the uploaded url

--- storage.py
+++ storage.py
@@ -1,8 +1,11 @@
+from os.path import join
+
 from django.core.files.storage import FileSystemStorage
 from django.conf import settings
+
 
 # Do not forget to set UPLOAD_ROOT and UPLOAD_URL inside you settings file
 upload_storage = FileSystemStorage(
     location=settings.UPLOAD_ROOT,
-    base_url=settings.UPLOAD_URL
+    base_url=join('file_explorer', settings.UPLOAD_URL)
 )
